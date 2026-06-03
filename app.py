c35c9c2ee87631297c44cdc2831efa0555d00083
programmers-pakisan <mahashah600@gmail.com>
2026-06-03 16:47:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,26 @@
 import streamlit as st
 import pickle
+import sklearn
 
-# Load Model and TF-IDF
 with open("fake_news_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 with open("tfidf.pkl", "rb") as f:
     tfidf = pickle.load(f)
 
-# App Title
 st.title("📰 Fake News Detector")
 
-st.write("Enter a news article below to check if it is Real or Fake.")
-
-# Input Box
 news = st.text_area("Enter News Article")
 
-# Button
 if st.button("Check News"):
 
-    if news.strip() == "":
-        st.warning("Please enter some news text.")
+    if news.strip() != "":
 
-    else:
-        # Convert text into TF-IDF features
         vector = tfidf.transform([news])
 
-        # Prediction
         prediction = model.predict(vector)
 
-        # Result
-      if prediction[0] == 0:
-    st.error("⚠️ Fake News")
-else:
-    st.success("✅ Real News")
+        if prediction[0] == 0:
+            st.error("⚠️ Fake News")
+        else:
+            st.success("✅ Real News")
